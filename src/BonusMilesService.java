097a52a7cbbf2d7438ac1875a30d2baf42aa1860
LLlOPT <llloptman@gmail.com>
2020-08-24 19:09:32 +0300

    Add class BonusMilesService Refactor class Main

---
+++ src/BonusMilesService.java
@@ -0,0 +1,9 @@
+public class BonusMilesService {
+    public int calculate (int cost){
+
+        int delimetr = 20;
+        int miles = cost / delimetr;
+
+        return miles;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,8 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Узнайте сколько бонусных Миль вы получите");
-        Scanner priceScaner = new Scanner(System.in);
-        System.out.println("Введите цену билета без копеек и валюты");
-
-        int price = Integer.parseInt(priceScaner.nextLine());
-        int delimetr = 20;
-        int miles = price / delimetr;
-
-
-        System.out.println("Вы получите " + miles +" миль"); //Когда нибудь нужно заморочиться и сделать норм окончание для миль в зависимости от результата
-
-
-
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
